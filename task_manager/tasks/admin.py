71773c74219aa1bbec4ec493e985409cb92b30c1
hdbv95 <hdbv95@gmail.com>
2025-02-17 11:15:49 -0500

    feat: register Task model in the admin

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'assigned_to', 'status', 'due_date', 'created_at')
+    list_filter = ('status', 'due_date')
+    search_fields = ('title', 'assigned_to__username')
+    ordering = ('due_date',)
