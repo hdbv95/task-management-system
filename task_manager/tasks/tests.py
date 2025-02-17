2dd514b6e6e1313c63afd8073e27b66469ac4bc1
hdbv95 <hdbv95@gmail.com>
2025-02-17 11:05:13 -0500

    feat: add tasks view, urls and tests

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Task
 
-# Create your tests here.
+class TaskTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_task(self):
+        data = {
+            "title": "Test Task",
+            "description": "This is a test",
+            "due_date": "2025-12-31",
+            "status": "pending",
+            "assigned_to": self.user.id
+        }
+        response = self.client.post("/api/tasks/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
